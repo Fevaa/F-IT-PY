47a171dc792a3f815f78176efc198c5e3ce57691
Fevaa <146878053+Fevaa@users.noreply.github.com>
2023-10-27 08:59:54 +0300

    Update task_1.py

--- laba_2/task_1.py
+++ laba_2/task_1.py
@@ -1,13 +1,12 @@
-money_capital = 20000  # Подушка безопасности
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
-months = 1
+money_capital = 20000
+salary = 5000
+spend = 6000
+increase = 0.05
+months = 0
 
 while money_capital >= 0 and spend <= money_capital + salary:
-    if months > 0:
-        spend *= (1 + increase)
-        money_capital -= spend - salary
-        months += 1
+    spend *= 1 + increase
+    money_capital -= spend - salary
+    months += 1
 
-print("Количество месяцев, которое можно протянуть без долгов:", months-1)
+print("Количество месяцев, которое можно протянуть без долгов:", months)
